4b08da98c091d89e327c51599fc22f2d9a858af4
DZYN19 <ffghas123@gmail.com>
2025-10-21 20:15:15 +0700

    Update and rename script.js to script2.js

--- script2.js
+++ script2.js
@@ -6,6 +6,8 @@ document.getElementById("TombolStart").onclick = function () {
   document.getElementById("mainH2").classList.remove("hidden");
   document.getElementById("mainH3").classList.add("slide-in");
   document.getElementById("mainH3").classList.remove("hidden");
+  document.getElementById("github").classList.add("slide-in");
+  document.getElementById("github").classList.remove("hidden");
   const myBtn = document.getElementById("TombolBio");
   myBtn.style.display = "inline-block";
   myBtn.classList.remove("hidden");
@@ -14,13 +16,7 @@ document.getElementById("TombolStart").onclick = function () {
   document.getElementById("tekan").style.display = "none";
 };
 
-document.getElementById("TombolBio").onclick = function () {
-  document.getElementById("kananBox").style.display = "block";
-  document.getElementById("kananBox").innerHTML = `
-                <span id="closeBox" style="cursor:pointer;float:right;font-size:1.5rem;font-weight:bold;">&times;</span>
-                <b><u>Ini Biodataku</u></b><br><br>Hi! Perkenalkan namaku <b>Rizky Dzaky Nur</b><br> Umurku 15 tahun<br> Aku punya hobi Menggambar kadang juga Badminton<br>Aku tinggal di Jawa Timur<br>
-            `;
-  document.getElementById("closeBox").onclick = function () {
-    document.getElementById("kananBox").style.display = "none";
-  };
-};
+  document.getElementById("TombolBio").addEventListener("click", function () {
+    var kananBox = document.getElementById("kananBox");
+    kananBox.classList.toggle("show");
+  });
